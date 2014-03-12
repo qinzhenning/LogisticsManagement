5507337433cb1cfb631238b36f5c904102f4c500
qinzhenning <qinzhenning@172.17.211.15>
2014-03-12 14:25:18 +0800

    test for two

---
+++ src/com/LogisticsManagement/logic/Order.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.LogisticsManagement.logic;
+
+/**
+ * @author qinzhenning
+ * 2014-3-12обнГ1:15:05
+ */
+public class Order {
+	private String oid;
+	private String status;
+}
