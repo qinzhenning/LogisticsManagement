90de5b89795c6a1ca5746f5d839c24a7a8522834
qinzhenning <qinzhenning@172.19.33.205>
2014-03-13 15:10:18 +0800

    建立主界面

---
+++ src/com/LogisticsManagement/GUI/MainFrame.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.LogisticsManagement.GUI;
+
+/**
+ * @author qinzhenning
+ * 2014-3-13下午2:40:02
+ */
+public class MainFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
--- src/com/LogisticsManagement/logic/Car.java
+++ src/com/LogisticsManagement/logic/Car.java
@@ -3,6 +3,8 @@
  */
 package com.LogisticsManagement.logic;
 
+import java.util.ArrayList;
+
 /**
  * @author qinzhenning
  * 2014-3-13下午2:02:18
@@ -10,5 +12,6 @@ package com.LogisticsManagement.logic;
 public class Car {
 	private int cid;//汽车编号
 	private CarsStatus cstatus;//汽车状态
+	private ArrayList<Integer> orlist;//订单编号列表
 	
 }
